cc352dac6521a32e9a6700f0b65f19bdeaaef86a
VanQuyet5H <phamhoangh7@gmail.com>
2021-09-27 08:19:48 +0700

    Test

---
+++ b.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+#define N 50
+struct queue
+{
+	int F;
+	int R;
+	int Q[N];
+};
+void QCEnQueue(Queue&Q.F,&Q.R;int x)
+	if(Q.F==1)&&(Q.R==n)||(Q.(R+1)==Q.F)
+		{ 
+			cout<<"hang doi da day";
+			return;
+		}
+	QF.Q[++QR.R]=x;
+Bool CQIsEmpty(Queue&Q.F,&Q.R);
+	{
+		return QF.F==-1;
+	}
+int CQDelete(Queue&Q.F,&Q.R)
+	if(QF.F==-1)
+		{
+			cout<<"Hang doi da rong";
+			return;
+		}
+	Tg=Q[F++];
+	CQDelete=Tg;
+	return;
+int main()
+{
+	Queue QF={-1};	
+	cout<<endl;
+	return 0;
+}====dinh nghia ham===	
+
